3a9fbf384dd0cccd8102801667a1610baabe9549
K Hong <albtrossfacefall@gmail.com>
2019-08-19 22:13:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names=[]
+  data.each do |attribute, attr_hash|
+    attr_hash.each do |detail_key,name_array|
+      name_array.each {|name| names.push(name)}
+    end
+  end
+  
+  finalhash={}
+  names.uniq.each do |name|
+    finalhash[name]={}
+  end
+  
+  data.each do |attribute, attr_hash|
+    finalhash.each do |name,hash|
+      hash[attribute]=[]
+      
+      attr_hash.each do |detail_key,name_array|
+        if name_array.include?(name)
+          finalhash[name][attribute].push(detail_key.to_s)
+        end
+      end
+    end
+    
+  end
+  
+  finalhash
 end
