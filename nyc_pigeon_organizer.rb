cc231fe2d562c09bc29d1a74509841255bcec07e
K Hong <albtrossfacefall@gmail.com>
2019-08-19 21:17:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,7 @@
 def nyc_pigeon_organizer(data)
-  data.each do |attribute, |
-    
+  data.each do |attribute, attr_hash|
+    attr_hash.each do |detail_key,name_array|
+      name_array.values
+    end
   end
 end
